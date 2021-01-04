795effc297a098080467f21b808169f44c9ca4c5
WON-DER <58491427+WON-DER@users.noreply.github.com>
2021-01-03 18:30:25 -0800

    Initial commit
    Initial commit for Weather App to deploy with Netlify.

---
+++ js/main.js
@@ -0,0 +1,115 @@
+let API_KEY = process.env.API_KEY;
+
+function onSubmit() {
+  getCity.value === ""
+    ? alert("Please enter a valid city name")
+    : (city = getCity.value),
+    (state = getState.value),
+    (city = toTitleCase(city)),
+    console.log(city, state);
+  getWeather(), (getCity.value = "");
+}
+async function getWeather() {
+  response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},USA&units=imperial&appid=${API_KEY}`
+  ).then((res) => res.json());
+  response["cod"] != 200
+    ? alert("Invalid city name, please try again.")
+    : setWeather(),
+    weatherTextColor();
+  console.log(response);
+}
+
+let toTitleCase = (text) =>
+  text
+    .toLowerCase()
+    .split(" ")
+    .map((text) => text.replace(text[0], text[0].toUpperCase()))
+    .join(" ");
+
+function weatherTextColor() {
+  tempCurrent.style.color = "rgb(140, 140, 153)";
+  tempMin.style.color = "rgb(140, 140, 153)";
+  tempMax.style.color = "rgb(140, 140, 153)";
+  windSpeed.style.color = "rgb(140, 140, 153)";
+  windDeg.style.color = "rgb(140, 140, 153)";
+
+  if (temp < 60) {
+    tempCurrent.style.color = "lightblue";
+  } else if (temp > 75) {
+    tempCurrent.style.color = "rgb(204, 61, 61)";
+  }
+  if (min < 60) {
+    tempMin.style.color = "lightblue";
+  } else if (min > 75) {
+    tempMin.style.color = "rgb(204, 61, 61)";
+  }
+  if (max < 60) {
+    tempMax.style.color = "lightblue";
+  } else if (max > 75) {
+    tempMax.style.color = "rgb(204, 61, 61)";
+  }
+  if (wind > 10 && wind < 20) {
+    windSpeed.style.color = "rgb(242, 245, 88)";
+    windDeg.style.color = "rgb(242, 245, 88)";
+  } else if (wind > 20) {
+    windSpeed.style.color = "rgb(204, 61, 61)";
+    windDeg.style.color = "rgb(204, 61, 61)";
+  }
+}
+
+function setWeather() {
+  cityOutput.innerHTML = `${city}, ${state}`;
+  temp = Math.round(response["main"]["temp"]);
+  weatherType = toTitleCase(response["weather"][0]["description"]);
+  min = Math.round(response["main"]["temp_min"]);
+  max = Math.round(response["main"]["temp_max"]);
+  wind = Math.round(response["wind"]["speed"]);
+  windDir = response["wind"]["deg"];
+  // getIcon();
+  getIcon();
+  displayResult();
+}
+
+function tempSwitch() {}
+
+function getIcon() {
+  icon = response["weather"]["0"]["icon"];
+  console.log(icon);
+}
+
+async function displayResult() {
+  tempCurrent.innerHTML = ` ${temp}&#176; ${tempChoice}`;
+  weatherIcon.setAttribute("alt", weatherType);
+  await weatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${icon}.png`
+  );
+  weatherConditions.innerHTML = ` ${weatherType}`;
+  tempMax.innerHTML = `${max}&#176; ${tempChoice}`;
+  tempMin.innerHTML = `${min}&#176; ${tempChoice}`;
+  windSpeed.innerHTML = `${wind} mph`;
+  windDeg.style.transform = `rotate(${windDir}deg)`;
+  outputGroup.classList.remove("hidden");
+}
+
+let submit = document
+  .querySelector(".submit")
+  .addEventListener("click", onSubmit);
+let getCity = document.querySelector(".cityRequest");
+let getState = document.querySelector(".stateRequest");
+let resultDisplay = document.querySelector(".output");
+let weatherIcon = document.querySelector(".weather-icon");
+let outputGroup = document.querySelector(".outputGroup");
+let cityOutput = document.querySelector(".cityOutput");
+let outputInfo = document.querySelector(".outputInfo");
+let outputCats = document.querySelector(".outputCats");
+let tempCurrent = document.querySelector(".tempCurrent");
+let weatherConditions = document.querySelector(".weatherType");
+let tempMax = document.querySelector(".tempMax");
+let tempMin = document.querySelector(".tempMin");
+let windSpeed = document.querySelector(".windSpeed");
+let windDeg = document.querySelector(".windDir");
+let response;
+let temp, icon, weatherType, city, max, min;
+let tempChoice = "f";
